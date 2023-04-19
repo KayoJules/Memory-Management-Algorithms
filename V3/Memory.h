8be6daeb9e9f635a0f289484ab4f228afab16499
Chaotic <114947140+KayoJules@users.noreply.github.com>
2023-04-19 01:06:32 -0500

    LRU Working & FIFO Fix

---
+++ V3/Memory.h
@@ -0,0 +1,48 @@
+#ifndef MEMORY_H_
+#define MEMORY_H_
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+class Memory {
+
+public:
+
+    Memory();
+    //Memory(int a, int b, int c);
+    Memory(vector<int> input, int size);
+
+    void print();
+    void printReference();
+    void push(int value);
+    void replace(int position, int value);
+    void reset();
+
+    void setSize(int n);
+    void setReference(vector<int> ref);
+    bool isIn(int n);
+    //bool isFull();
+
+    void fifo();                             // First in first out
+    void optimal();                          // Optimal algorithm
+    void lru();                              // Least recently used
+    int distance(int n);
+
+    int lruPosition(map<int, int> m);
+
+
+private:
+
+    //int* frame = new int[size];
+    vector<int> frame;
+    vector<int> reference;
+
+    int frameSize = NULL;
+    int faults = 0;
+};
+
+#endif
